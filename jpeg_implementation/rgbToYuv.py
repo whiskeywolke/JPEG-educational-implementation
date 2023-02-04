0a9c31315f2462dac44931db3c1a15433ebaa648
Michael M <mentemichael@live.com>
2023-02-04 13:29:31 +0100

    add yuv colormaps

--- jpeg_implementation/rgbToYuv.py
+++ jpeg_implementation/rgbToYuv.py
@@ -1,6 +1,7 @@
-import cv2
+import cv2 # pip install opvencv-python
+
 
 def rgb_to_yuv(image):
     yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-    y,u,v = cv2.split(yuv)
-    return y*255,u*255,v*255
+    y, u, v = cv2.split(yuv)
+    return y * 255, u * 255, v * 255
