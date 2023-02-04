4ba510025e967ae49450e07199d54f31869eb0fe
Michael M <63014706+whiskeywolke@users.noreply.github.com>
2023-02-04 19:17:27 +0100

    add reverse z traversal, run length decoding

--- jpeg_implementation/runLengthEncode.py
+++ jpeg_implementation/runLengthEncode.py
@@ -1,7 +1,11 @@
-def resort_values(block, block_size):
+import numpy as np
+
+
+def resort_values_zig_zag(block, block_size):
     new_block = []
     for diag in range(2 * block_size - 1):
-        direction = diag % 2 == 0
+        # direction = diag % 2 == 0
+        direction = True
         if diag < block_size:
             if direction:
                 x = 0
@@ -29,6 +33,22 @@ def resort_values(block, block_size):
     return new_block
 
 
+def resort_values_zig_zag_reverse(block, block_size):
+    # to reverse the process of zigzag iteration, give incremental list to zigzag iteration
+    # then find indices of incremental values
+    # then use indices to reverse reordering
+    range_block = np.reshape(range(block_size ** 2), newshape=(block_size, block_size))
+    range_block_zz = resort_values_zig_zag(range_block, block_size)
+    indices = [range_block_zz.index(i) for i in range(block_size ** 2)]
+
+    new_block = []
+
+    for index in indices:
+        new_block.append(block[index])
+
+    return np.reshape(new_block, (block_size, block_size))
+
+
 def run_length_encode(input_vals):
     encoded_list = []
     current = input_vals[0]
@@ -46,6 +66,13 @@ def run_length_encode(input_vals):
     return encoded_list
 
 
+def run_length_decode(encoded):
+    decoded = []
+    for val, count in encoded:
+        decoded += [val] * count
+    return decoded
+
+
 def flatten(encoded):
     res = []
     for val in encoded:
@@ -54,11 +81,159 @@ def flatten(encoded):
     return res
 
 
+def un_flatten(flattened):
+    assert len(flattened) % 2 == 0
+    res = []
+    for i in range(len(flattened) // 2):
+        res.append((flattened[i * 2], flattened[i * 2 + 1]))
+    return res
+
+
+# def resort_and_run_length_encode(color_component, block_size):
+#     encoded_blocks = []
+#     for block in color_component:
+#         resorted = resort_values_zig_zag(block, block_size)
+#         run_length_encoded = run_length_encode(resorted)
+#         flattened = flatten(run_length_encoded)
+#         encoded_blocks += flattened
+#     return encoded_blocks
+
+
 def resort_and_run_length_encode(color_component, block_size):
-    encoded_blocks = []
+    resorted_blocks_flattened = []
     for block in color_component:
-        resorted = resort_values(block, block_size)
-        run_length_encoded = run_length_encode(resorted)
-        flattened = flatten(run_length_encoded)
-        encoded_blocks += flattened
-    return encoded_blocks
+        resorted = resort_values_zig_zag(block, block_size)
+        resorted_blocks_flattened += resorted
+    encoded_blocks = run_length_encode(resorted_blocks_flattened)
+    encoded_blocks_flattened = flatten(encoded_blocks)
+    return encoded_blocks_flattened
+
+
+def unsort_and_run_length_decode(color_component, block_size):
+    un_flattened = un_flatten(color_component)
+    rld = run_length_decode(un_flattened)
+    block_element_count = block_size ** 2
+    assert len(rld) % block_element_count == 0
+
+    blocks = []
+    for i in range(len(rld[::block_element_count])):
+        block_zig_zag = rld[i * block_element_count: (i + 1) * block_element_count]
+        block = resort_values_zig_zag_reverse(block_zig_zag, block_size)
+        blocks.append(block)
+
+    return blocks
+
+
+def test():
+    quantized1 = [
+        [
+            [6., 1., -1., 0., -0., 0., -0., -0.],
+            [0., -0., 0., 0., -0., -0., -0., 0.],
+            [0., -0., 0., -0., 0., -0., 0., -0.],
+            [0., -0., 0., -0., -0., 0., 0., -0.],
+            [-0., 0., -0., -0., 0., -0., -0., 0.],
+            [-0., 0., 0., 0., -0., 0., 0., -0.],
+            [-0., 0., -0., 0., 0., -0., -0., 0.],
+            [-0., 0., -0., 0., -0., 0., 0., -0.],
+        ],
+        [
+            [5., -0., -0., -0., 0., -0., -0., -0.],
+            [1., 0., -0., -0., -0., 0., 0., 0.],
+            [0., 0., 0., 0., -0., -0., -0., 0.],
+            [-0., -0., 0., 0., 0., 0., -0., 0.],
+            [-0., -0., 0., -0., 0., 0., 0., -0.],
+            [0., -0., -0., -0., 0., -0., 0., 0.],
+            [0., -0., 0., 0., -0., -0., 0., 0.],
+            [-0., -0., 0., -0., -0., 0., -0., -0.],
+        ],
+        [
+            [4., 0., 1., -1., -0., -0., 0., -0.],
+            [0., 1., -0., 0., 0., 0., 0., 0.],
+            [0., 0., -0., 0., 0., 0., -0., 0.],
+            [-0., 0., -0., 0., 0., 0., -0., -0.],
+            [-0., 0., -0., 0., -0., 0., -0., -0.],
+            [0., 0., -0., 0., 0., 0., 0., 0.],
+            [0., 0., -0., 0., 0., 0., -0., -0.],
+            [-0., 0., -0., 0., 0., 0., -0., -0.],
+        ],
+        [
+            [5., 1., 1., 0., -0., -0., -0., -0.],
+            [-0., 0., -0., -0., -0., 0., -0., -0.],
+            [0., -0., 0., -0., -0., -0., 0., 0.],
+            [-0., -0., 0., -0., 0., -0., 0., -0.],
+            [-0., 0., -0., 0., 0., 0., -0., -0.],
+            [0., 0., 0., 0., -0., 0., -0., -0.],
+            [0., 0., -0., -0., 0., -0., 0., 0.],
+            [0., -0., -0., 0., -0., 0., -0., 0.]
+        ]
+    ]
+
+    quantized2 = [
+        [[-1., -0., 0., -0., -0., 0., 0., 0.],
+         [-0., 0., -0., -0., 0., -0., -0., 0.],
+         [0., -0., 0., -0., -0., 0., -0., -0.],
+         [-0., 0., 0., -0., 0., -0., 0., -0.],
+         [0., 0., -0., 0., 0., -0., -0., 0.],
+         [0., -0., 0., -0., 0., 0., 0., -0.],
+         [-0., 0., -0., 0., -0., 0., 0., -0.],
+         [0., -0., -0., -0., -0., -0., -0., 0.], ],
+
+        [[-1., -0., 0., 0., -0., 0., 0., 0.],
+         [-0., -0., 0., -0., -0., 0., -0., 0.],
+         [-0., 0., -0., -0., 0., -0., 0., 0.],
+         [-0., 0., -0., 0., -0., -0., 0., -0.],
+         [-0., -0., -0., 0., -0., 0., -0., 0.],
+         [0., 0., 0., 0., -0., 0., 0., -0.],
+         [-0., -0., -0., -0., -0., 0., -0., -0.],
+         [0., 0., 0., 0., -0., 0., -0., 0.], ],
+
+        [[-0., -0., -1., 0., 0., 0., 0., 0.],
+         [-0., -0., 0., 0., -0., -0., -0., -0.],
+         [-0., 0., -0., 0., -0., -0., 0., -0.],
+         [0., -0., 0., 0., -0., -0., 0., 0.],
+         [-0., -0., 0., -0., -0., -0., 0., 0.],
+         [-0., -0., 0., 0., -0., 0., -0., 0.],
+         [-0., -0., -0., 0., -0., 0., -0., 0.],
+         [0., -0., 0., 0., -0., -0., 0., -0.], ],
+
+        [[-1., 0., -1., -0., 0., 0., 0., -0.],
+         [0., 0., -0., 0., -0., 0., -0., 0.],
+         [0., -0., 0., 0., 0., 0., -0., 0.],
+         [0., 0., -0., 0., 0., -0., 0., -0.],
+         [0., 0., 0., -0., -0., 0., 0., 0.],
+         [-0., 0., 0., -0., 0., -0., 0., 0.],
+         [0., -0., 0., 0., 0., 0., 0., -0.],
+         [0., 0., 0., -0., 0., -0., 0., 0.], ],
+    ]
+
+    quantized3 = [
+        [
+            [0, 1],
+            [2, 3]
+        ],
+        [
+            [4, 5],
+            [6, 7]
+        ]
+    ]
+
+    # rle = resort_and_run_length_encode(quantized3, 2)
+    # res = unsort_and_run_length_decode(rle, 2)
+    # print(len(res), len(quantized3))
+    # print(res)
+    # print(quantized3)
+    # print(np.array_equal(res, quantized3))
+
+    rle = resort_and_run_length_encode(quantized2, 8)
+    res = unsort_and_run_length_decode(rle, 8)
+    print(np.array_equal(res, quantized2))
+
+    # random_list = np.random.random_sample(size=64).reshape((8, 8))
+    # a = resort_values_zig_zag(random_list, 8)
+    # b = resort_values_zig_zag_reverse(a, 8)
+    # print(b)
+    # # print(list(random_list.reshape(1,64)[0]))
+    # # print(list(random_list.reshape(1,64)[0]) == b)
+
+
+test()
